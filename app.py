837327f9b57db0f4135db156901765bf3faf0b76
Fajar Alif <alifrdu@gmail.com>
2019-09-13 04:35:01 +0700

    add send sms notification using twillio

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+from twilio.rest import Client
+
 import os
 import requests
 
 import settings
 
 
+# Twitch
 CLIENT_ID = os.getenv('CLIENT_ID')
+
+
+# Twillio
+ACCOUNT_SID = os.getenv('TWILLIO_ACCOUNT_SID')
+AUTH_TOKEN = os.getenv('TWILLIO_AUTH_TOKEN')
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 
 # Gets the user objects for the specified Twitch login names
@@ -34,7 +43,25 @@ def get_stream(user_id):
 	return r.json()
 
 
+def send_notification(streams):
+	message_from = os.getenv('TWILLIO_FROM')
+	message_to = os.getenv('TWILLIO_TO')
+	message_body = login + ' is streaming, go watch it'
+
+	message = client.messages \
+		.create(
+			body=message_body,
+			from_=message_from,
+			to=message_to
+		)
+
+	return message.sid
+
 if __name__ == '__main__':
-	login = '<Twitch Name>'
+	login = 'streamer_name'
 	user_id = get_user_id(login)['users'][0]['_id']
-	print(get_stream(user_id))
+	streams = get_stream(user_id)
+	if streams['stream'] is None:
+		print(login + ' is not streaming right now')
+	else:
+		send_notification(streams)
